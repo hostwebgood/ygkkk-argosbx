1395749984a14ce6571240aef8ef8ea3b5c58298
hostwebgood <hostsc@126.com>
2026-03-07 19:46:43 +0800

    Change agsbxurl to new GitHub repository link
    Updated agsbxurl to point to a new GitHub repository.

--- argosbx3.sh
+++ argosbx3.sh
@@ -41,7 +41,7 @@ export warp=${warp:-''}
 export name=${name:-''}
 export oap=${oap:-''}
 v46url="https://icanhazip.com"
-agsbxurl="https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh"
+agsbxurl="https://github.com/hostwebgood/ygkkk-argosbx/raw/refs/heads/main/argosbx3.sh"
 showmode(){
 echo "Argosbx脚本一键SSH命令生器在线网址：https://yonggekkk.github.io/argosbx/"
 echo "主脚本：bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh) 或 bash <(wget -qO- https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh)"
