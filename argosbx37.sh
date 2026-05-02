454386d443816d3bc40c16018cd3f5f11f51c55f
hostwebgood <hostsc@126.com>
2026-05-02 12:16:32 +0800

    Update argosbx37.sh

--- argosbx37.sh
+++ argosbx37.sh
@@ -245,7 +245,7 @@ fi
 if [ -n "$vxp" ]; then
 vxp=vxpt
 if [ ! -e "$HOME/agsbx/sskey" ]; then
-sskey=${sskey:-"$(openssl rand -base64 16)"}
+sskey=$(openssl rand -base64 16)
 echo "$sskey" > "$HOME/agsbx/sskey"
 fi
 if [ -z "$port_vx" ] && [ ! -e "$HOME/agsbx/port_vx" ]; then
